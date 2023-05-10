2c57257d2ab8b88c5e4a9cf45d64c985e21eadba
JungInLee0130 <dlwjddls0130@naver.com>
2023-05-10 09:08:12 +0900

    commit

---
+++ src/views/MemberView.vue
@@ -0,0 +1,19 @@
+<template>
+    
+</template>
+
+<script>
+export default {
+    name: '',
+    components: {},
+    data() {
+        return {
+            message: '',
+        };
+    },
+    created() {},
+    methods: {},
+};
+</script>
+
+<style scoped></style>
